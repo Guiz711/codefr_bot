1bfd3cb926b011c0fad033da3a812f8bfdb200ba
Guillaume Michaud <gmichaud@student.42.fr>
2018-03-12 20:46:48 +0100

    changed comments in index.js

--- index.js
+++ index.js
@@ -4,27 +4,17 @@ const botToken = require('./json_resources/bot_token.json');
 const botConfig	= require('./json_resources/bot_config.json');
 const CmdHandler = require('./CmdHandler.js');
 
+/*
+** Bot initialization and events handling.
+*/
+
 CmdHandler.setVerbose();
 const handler = new CmdHandler('commands');
 handler.loadAllCommands();
 
-
-/**
- * ANG: When the bot is launched.
- * FRA: Lorsque le bot est lancé.
- */
 client.on('ready', () => console.log('Discord bot ready'));
-
-/**
- * ANG: When an error comes from discord.js.
- * FRA: Lorsque une erreur vient de discord.js.
- */
 client.on('error', err => console.error(err));
 
-/**
- * ANG: When the bot receives a message.
- * FRA: Lorsque le bot reçoit un message.
- */
 client.on('message', msg => {
     try {
         handler.treatMessage(client, msg);
