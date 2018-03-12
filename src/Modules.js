040202e9a3b91485bcd7414b871aee3faf24460d
LightDiscord <arnaud@lightdiscord.me>
2018-03-12 12:08:03 +0100

    Use babel, decorators and src folder

--- src/Modules.js
+++ src/Modules.js
@@ -7,14 +7,5 @@ export const commands = new Map();
 export const listeners = new Map();
 
 export function load () {
-/*    for ([name, Module] of Object.entries(require('./modules/'))) {
-        const instance = new Module()
-        Module.prototype[INSTANCE] = instance
-        instance.name = Module.name
-
-        console.log("coucou")
-        modules.push(instance)
-    }*/
-
     require('./modules')
 }
