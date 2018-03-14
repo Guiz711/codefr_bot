243c501abbfecd1f79535a86ec8813caffd094df
hichxm <hicham.slimani.fr@gmail.com>
2018-03-14 21:54:03 +0100

    Edit help.js file to include list of commands when user tape !help

--- commands/help.js
+++ commands/help.js
@@ -1,14 +1,37 @@
-const Discord = require('discord.js');
+const config = require("./../libs/config");
+const logger = require("./../libs/logger");
 
 class help {
+
 	constructor() {
-		console.log('test command created');
+		logger.info("Help command initialised");
+		this.helpFile = require("./../json_resources/help.json");
 	}
 
 	run(client, message, args) {
-		message.channel.send('This is the help command.')
+
+		let template = "";
+
+		template += "```yaml\n";
+		template += "├─ La commande help, retourne toute les commandes disponible.\n";
+		template += "│\n";
+
+		this.helpFile.commands.forEach((command, index) => {
+			let maxCommand = this.helpFile.commands.length - 1;
+
+			if (index !== maxCommand) {
+				template += "├───" + config.cmd_prefix + command.name + " \t- " + command.desc + "\n";
+			} else {
+				template += "└───" + config.cmd_prefix + command.name + " \t- " + command.desc + "\n";
+			}
+        });
+
+		template += "```\n";
+
+		message.channel.send(template)
 			.catch(console.error);
 	}
+
 }
 
 module.exports = help;
