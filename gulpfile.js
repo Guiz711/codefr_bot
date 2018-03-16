5a413e762ade2196d8477ad96423a5b49c8ef982
hichxm <hicham.slimani.fr@gmail.com>
2018-03-16 19:15:04 +0100

    New task for verify the project

--- gulpfile.js
+++ gulpfile.js
@@ -12,8 +12,28 @@ const JsBeautifier = require("gulp-jsbeautifier");
  * This task makes the code cleaner for the entire project.
  */
 gulp.task("beautifier", function() {
-	gulp.src(['./**/*.js'])
+	return gulp.src(['./**/*.js', '!./node_modules/**'])
 		.pipe(JsBeautifier({
+			js: {
+				indent_char: "\t",
+				indent_size: 1
+			}
+		}))
+		.pipe(gulp.dest("./"));
+});
+
+/**
+ * This task verify the code cleaner for the entire project.
+ */
+gulp.task("verify", function() {
+	return gulp.src(['./**/*.js', '!./node_modules/**'])
+		.pipe(JsBeautifier.validate({
+			js: {
+				indent_char: "\t",
+				indent_size: 1
+			}
+		}))
+		.pipe(JsBeautifier.reporter({
 			js: {
 				indent_char: "\t",
 				indent_size: 1
