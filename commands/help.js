64cb97162520ab0f7b9301b18782670f19bbcbdd
Hicham'S <hicham.slimani.fr@gmail.com>
2018-03-16 18:46:51 +0100

    New Help command, list all commands. (#16)
    * Edit help.js file to include list of commands when user tape !help
    
    * Map file for commands
    
    * fix promises
    
    * fix name for better comprehension.
    
    * fix. language

--- commands/help.js
+++ commands/help.js
@@ -1,14 +1,50 @@
-const Discord = require('discord.js');
+const config = require("./../libs/config");
+const logger = require("./../libs/logger");
 
 class help {
+
 	constructor() {
-		console.log('test command created');
+		logger.info("Help command initialised");
+		this.helpFile = require("./../json_resources/commands.json");
 	}
 
 	run(client, message, args) {
-		message.channel.send('This is the help command.')
-			.catch(console.error);
+
+		let template = "";
+
+		template += "```yaml\n";
+		template += "├─ Voici la liste des commandes.\n";
+		template += "│\n";
+
+		this.helpFile.commands.forEach((command, index) => {
+			let maxCommand = this.helpFile.commands.length - 1;
+
+			if (index !== maxCommand) {
+				template += "├───" + config.cmd_prefix + command.name + " \t- " + command.desc + "\n";
+			} else {
+				template += "└───" + config.cmd_prefix + command.name + " \t- " + command.desc + "\n";
+			}
+        });
+
+		template += "```\n";
+
+		message.channel.send(template)
+
+			/**
+			 * When the shipment went well.
+			 */
+            .then(function () {
+                logger.info("Help executed by " + message.author.tag);
+            })
+
+            /**
+             * When an error has occurred.
+             */
+            .catch(function (err) {
+                logger.info("Error" + err.stderr);
+            })
 	}
+
 }
 
 module.exports = help;
