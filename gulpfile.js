f3b8bd01e0612176544875dd990f0e25d92f9be9
hichxm <hicham.slimani.fr@gmail.com>
2018-03-16 20:04:09 +0100

    Gulp can now indent json files.

--- gulpfile.js
+++ gulpfile.js
@@ -12,9 +12,13 @@ const JsBeautifier = require("gulp-jsbeautifier");
  * This task makes the code cleaner for the entire project.
  */
 gulp.task("beautifier", function() {
-	return gulp.src(['./**/*.js', '!./node_modules/**'])
+	return gulp.src(['./**/*.js', './**/*.json', '!./node_modules/**'])
 		.pipe(JsBeautifier({
 			js: {
+				indent_char: "\t",
+				indent_size: 1
+			},
+			json: {
 				indent_char: "\t",
 				indent_size: 1
 			}
@@ -26,15 +30,23 @@ gulp.task("beautifier", function() {
  * This task verify the code cleaner for the entire project.
  */
 gulp.task("verify", function() {
-	return gulp.src(['./**/*.js', '!./node_modules/**'])
+	return gulp.src(['./**/*.js', './**/*.json', '!./node_modules/**'])
 		.pipe(JsBeautifier.validate({
 			js: {
+				indent_char: "\t",
+				indent_size: 1
+			},
+			json: {
 				indent_char: "\t",
 				indent_size: 1
 			}
 		}))
 		.pipe(JsBeautifier.reporter({
 			js: {
+				indent_char: "\t",
+				indent_size: 1
+			},
+			json: {
 				indent_char: "\t",
 				indent_size: 1
 			}
